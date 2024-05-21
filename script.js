d4ab636945fbd6b4e3a4f58ec878e937a896481c
misogyu <avlis828@gmail.com>
2024-05-21 10:37:47 -0700

    revised

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+let didScroll;
+let delta = 5;
+let navbarHeight = header.offsetHeight;
+
 window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    header.classList.toggle('scrolled', window.scrollY > 0);
+    didScroll = true;
 });
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let st = window.pageYOffset;
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If scrolled down and past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        header.style.transform = "translateY(-100%)";
+    } else {
+        // Scroll Up
+        if(st + window.innerHeight < document.body.scrollHeight) {
+            header.style.transform = "translateY(0)";
+        }
+    }
+
+    lastScrollTop = st;
+}
