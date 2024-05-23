b9390bdeff2932004c55ffbbf06537dd64aad934
misogyu <avlis828@gmail.com>
2024-05-22 20:10:08 -0700

    revised

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-let lastScrollTop = 0;
-const header = document.querySelector('.header');
-let didScroll;
-let delta = 5;
-let navbarHeight = header.offsetHeight;
-
-window.addEventListener('scroll', function() {
-    didScroll = true;
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    let st = window.pageYOffset;
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If scrolled down and past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        header.style.transform = "translateY(-100%)";
-    } else {
-        // Scroll Up
-        if(st + window.innerHeight < document.body.scrollHeight) {
-            header.style.transform = "translateY(0)";
-        }
-    }
-
-    lastScrollTop = st;
-}
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
